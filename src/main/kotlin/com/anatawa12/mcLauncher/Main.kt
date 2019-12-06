58815c5fe5b8933f92af6197f8414e5bba9cf30b
anatawa12 <anatawa12@icloud.com>
2019-12-06 22:34:39 +0900

    add loadLaunchInfo

--- src/main/kotlin/com/anatawa12/mcLauncher/Main.kt
+++ src/main/kotlin/com/anatawa12/mcLauncher/Main.kt
@@ -1,8 +1,21 @@
 package com.anatawa12.mcLauncher
 
+import com.anatawa12.mcLauncher.json.DateJsonAdapter
+import com.anatawa12.mcLauncher.json.VersionJson
+import com.anatawa12.mcLauncher.launchInfo.LaunchInfo
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import java.io.File
+import java.io.FileNotFoundException
 
 object Main {
+    val moshi = Moshi.Builder()
+        .add(DateJsonAdapter)
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    val versionJsonAdapter = moshi.adapter(VersionJson::class.java)
+
     lateinit var homeDir: File
         private set
     lateinit var appDataDir: File
@@ -11,6 +24,8 @@ object Main {
     @JvmStatic
     fun main(args: Array<String>) {
         init()
+
+        println(loadLaunchInfo(args[0]))
     }
 
     private fun init() {
@@ -28,5 +43,35 @@ object Main {
             }
         }
         appDataDir = File(appDataDirPath)
+    }
+
+    fun loadLaunchInfo(version: String): LaunchInfo {
+        val loadedVersions = mutableListOf<VersionJson>()
+        var versionJsonVersion: String? = version
+        while (versionJsonVersion != null) {
+            if (loadedVersions.any { it.id == versionJsonVersion })
+                knownError("'inheritsFrom' is looping! entry version is '$version'")
+
+            val loadedVersionJson = loadVersionJson(versionJsonVersion)
+            loadedVersions += loadedVersionJson
+
+            versionJsonVersion = loadedVersionJson.inheritsFrom
+        }
+
+        val builder = LaunchInfo.Builder(version)
+
+        loadedVersions.reversed().forEach(builder::addVersionJson)
+
+        return builder.build()
+    }
+
+    fun loadVersionJson(version: String): VersionJson {
+        val jsonFile = appDataDir.resolve("versions").resolve(version).resolve("$version.json")
+
+        try {
+            return versionJsonAdapter.fromJson(jsonFile.readText()) ?: knownError("$version.json is valid")
+        } catch (e: FileNotFoundException) {
+            knownError("$version.json is not a file.", e)
+        }
     }
 }
