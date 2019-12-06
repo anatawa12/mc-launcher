c8ea172612e371fd9bfe6b8881805fb5eddad958
anatawa12 <anatawa12@icloud.com>
2019-12-06 22:08:53 +0900

    add init

--- src/main/kotlin/com/anatawa12/mcLauncher/Main.kt
+++ src/main/kotlin/com/anatawa12/mcLauncher/Main.kt
@@ -1,7 +1,32 @@
 package com.anatawa12.mcLauncher
 
+import java.io.File
+
 object Main {
+    lateinit var homeDir: File
+        private set
+    lateinit var appDataDir: File
+        private set
+
     @JvmStatic
     fun main(args: Array<String>) {
+        init()
+    }
+
+    private fun init() {
+        homeDir = File(System.getProperty("user.home"))
+        val appDataDirPath: String
+        when (Platform.current) {
+            Platform.Linux -> {
+                appDataDirPath = "$homeDir/.minecraft"
+            }
+            Platform.MacOS -> {
+                appDataDirPath = "$homeDir/Library/Application Support/minecraft"
+            }
+            Platform.Windows -> {
+                appDataDirPath = "${System.getenv("APPDATA")}\\.minecraft"
+            }
+        }
+        appDataDir = File(appDataDirPath)
     }
 }
