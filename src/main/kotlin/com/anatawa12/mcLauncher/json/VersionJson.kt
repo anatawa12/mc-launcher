2a58a532d6cf2f230559843612950d8a6a1ab202
anatawa12 <anatawa12@icloud.com>
2019-12-07 08:42:48 +0900

    support 1.12.2 forge json

--- src/main/kotlin/com/anatawa12/mcLauncher/json/VersionJson.kt
+++ src/main/kotlin/com/anatawa12/mcLauncher/json/VersionJson.kt
@@ -4,14 +4,14 @@ import java.util.*
 
 data class VersionJson(
     val assetIndex: AssetIndex? = null,
-    val assets: String,
+    val assets: String? = null,
     val downloads: Map<String, DownloadFile>? = null,
     val id: String,
     val libraries: List<Library> = listOf(),
     val logging: Map<String, Logging>? = null,
     val mainClass: String,
     val minecraftArguments: String,
-    val minimumLauncherVersion: Int,
+    val minimumLauncherVersion: Int? = null,
     val releaseTime: Date,
     val time: Date,
     val type: String,
