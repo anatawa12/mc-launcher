9d3c9dd3ce2082a1ad89bea86c91877fcf80cb12
anatawa12 <anatawa12@icloud.com>
2020-04-11 17:21:54 +0900

    add message pararm to subclasses of KnownErrorException

--- src/main/kotlin/com/anatawa12/mcLauncher/KnownError.kt
+++ src/main/kotlin/com/anatawa12/mcLauncher/KnownError.kt
@@ -5,18 +5,28 @@ sealed class KnownErrorException : Exception {
     constructor(message: String) : super(message)
     constructor(message: String, cause: Throwable?) : super(message, cause)
 
-    class InvalidVersionJson(version: String, cause: Throwable? = null) :
-        KnownErrorException("$version.json is invalid", cause)
+    class InvalidVersionJson(version: String, message: String? = null, cause: Throwable? = null) :
+        KnownErrorException("$version.json is invalid${message?.let { ": $it" } ?: ""}", cause) {
+        constructor(version: String, cause: Throwable? = null) : this(version, null, cause = cause)
+    }
 
-    class InvalidVersionJsonData(version: String, cause: Throwable? = null) :
-        KnownErrorException("$version.json has invalid data", cause)
+    class InvalidVersionJsonData(version: String, message: String? = null, cause: Throwable? = null) :
+        KnownErrorException("$version.json has invalid data${message?.let { ": $it" } ?: ""}", cause) {
+        constructor(version: String, cause: Throwable? = null) : this(version, null, cause = cause)
+    }
 
-    class InvalidLibraries(id: String, cause: Throwable? = null) :
-        KnownErrorException("library data for '$id' is invalid", cause)
+    class InvalidLibraries(id: String, message: String? = null, cause: Throwable? = null) :
+        KnownErrorException("library data for '$id' is invalid${message?.let { ": $it" } ?: ""}", cause) {
+        constructor(id: String, cause: Throwable? = null) : this(id, null, cause = cause)
+    }
 
-    class VersionJsonNotFile(version: String, cause: Throwable) :
-        KnownErrorException("$version.json is not a file.", cause)
+    class VersionJsonNotFile(version: String, message: String? = null, cause: Throwable) :
+        KnownErrorException("$version.json is not a file.${message?.let { ": $it" } ?: ""}", cause) {
+        constructor(version: String, cause: Throwable) : this(version, null, cause = cause)
+    }
 
-    class InvalidLibrary(libraryPath: String, cause: Throwable? = null) :
-        KnownErrorException("$libraryPath is not valid.", cause)
+    class InvalidLibrary(libraryPath: String, message: String? = null, cause: Throwable? = null) :
+        KnownErrorException("$libraryPath is not valid.${message?.let { ": $it" } ?: ""}", cause) {
+        constructor(libraryPath: String, cause: Throwable? = null) : this(libraryPath, null, cause = cause)
+    }
 }
